30515c29d4657f80c23bd2de4efebf6412f21c0e
Daku1 <80529485+Daku1@users.noreply.github.com>
2021-05-10 22:44:10 +0530

    menu and featured updated

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
 import Hero from './components/Hero'
 import { GlobalStyle } from './globalStyle'
-
+import { Products } from './components/Products'
+import { ProductData } from './components/Products/data'
+import { Feature } from './components/Feature'
 function App() {
-  return (
-    <Router>
-      <GlobalStyle />
-      <Hero />
-    </Router>
-  )
+    return (
+        <Router>
+            <GlobalStyle />
+            <Hero />
+            <Products heading="Choose your Favourite" data={ProductData} />
+            <Feature />
+        </Router>
+    )
 }
 
 export default App
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,7 +5,7 @@ const Navbar = ({ toggle }) => {
     return (
         <>
             <Nav>
-                <NavLink to="/">Dosae</NavLink>
+                <NavLink to="/">Dosaeria</NavLink>
                 <NavIcon onClick={toggle}>
                     <p>Menu</p>
                     <Bars />
